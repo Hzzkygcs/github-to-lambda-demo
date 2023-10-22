fbe917b06a81a6bf2abfffaf1b16292bd5244fac
Immanuel <immanuel.nuel02@gmail.com>
2023-10-23 01:31:36 +0700

    fix buildspec.yml

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# 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
+
 def lambda_handler(event, context):
     d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
